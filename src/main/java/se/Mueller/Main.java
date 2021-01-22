6acb4974fd697388aa66539c8b976b6462d08b45
JannisMueller <jannis.mueller@hotmail.de>
2021-01-22 11:56:22 +0100

    final changes

--- src/main/java/se/Mueller/Main.java
+++ src/main/java/se/Mueller/Main.java
@@ -9,17 +9,15 @@ public class Main {
 
         UserDao userDao = new UserDaoWithJPAImpl();
 
-        userDao.createUser("500603-9238", "janmue", "rest", "Jannis", "Mueller", "test@test.se", "0723068922");
+        userDao.createUser("500603-9838", "janmue", "rest", "Jannis", "TestName", "test@test.se", "0723068922");
 
         System.out.println(userDao.findByFirstName("Jannis"));
-
         System.out.println(userDao.findByLastName("Knutson"));
+        System.out.println(userDao.updatePhoneNumber("500603-9838", "updatedPhoneNumber"));
+        System.out.println(userDao.findByLastName("TestName"));
+        System.out.println(userDao.removeUser("500603-9838"));
 
-        System.out.println(userDao.updatePhoneNumber("500603-4238", "updatedPhoneNumber"));
 
-        System.out.println(userDao.removeUser("500603-8238"));
-
-        System.out.println(userDao.findByFirstName("Jannis"));
 
 
     }
